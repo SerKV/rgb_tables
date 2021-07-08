0711c57a0b5bca2e0b260b05e458a30ea8b38e08
SerKV <67077164+SerKV@users.noreply.github.com>
2021-07-08 22:31:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
+const td=<td>item</td>
 
-function App() {
-  return (
-    <div className="App">
-			Hello MY test
-    </div>
-  );
+function App () {
+	return (
+		<>
+			<table className="table red">
+				<tbody>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				</tbody>
+			</table>
+
+			<table className="table green">
+				<tbody>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				<tr>
+					{td}
+					{td}
+					{td}
+				</tr>
+				</tbody>
+			</table>
+
+			<table className="table blue">
+				<tbody>
+				<tr>
+					<td>item</td>
+					<td>item</td>
+					<td>item</td>
+				</tr>
+				<tr>
+					<td>item</td>
+					<td>item</td>
+					<td>item</td>
+				</tr>
+				<tr>
+					<td>item</td>
+					<td>item</td>
+					<td>item</td>
+				</tr>
+				</tbody>
+			</table>
+		</>
+	);
 }
 
 export default App;
